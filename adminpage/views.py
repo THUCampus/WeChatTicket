c26f14cc83f39f4a175a3189d6f02bce1064c2f5
7czzl <zzl850783164@163.com>
2018-10-14 23:48:49 +0800

    /activity/create & delete

--- adminpage/views.py
+++ adminpage/views.py
@@ -19,6 +19,16 @@ class adminLogin(APIView):
         self.check_input('username', 'password')
         username = self.input['username']
         password = self.input['password']
+        # try:
+        #     user = auth.models.User.objects.filter(username__exact = username)
+        #     if not user:
+        #         raise ValidateError("User name does not exit!")
+        #     if user.first().password != password:
+        #         raise ValidateError("User password error!")
+        #     user = auth.authenticate(username=username,password=password)
+        #     auth.login(self.request, user)
+        # except:
+        #     raise ValidateError("System error!")
         try:
             user = auth.authenticate(username=username,password=password)
             auth.login(self.request, user)
@@ -58,3 +68,48 @@ class activityList(APIView):
             return activity_List
         else:
             raise ValidateError('Please login!')
+
+#创建活动
+class activityCreate(APIView):
+    def post(self):
+        if self.request.user.is_authenticated():
+            self.check_input('name', 'key', 'place', 'description', 'picUrl', 'startTime', 'endTime', 
+            'bookStart', 'bookEnd', 'totalTickets', 'status')
+            
+            name = self.input['name']
+            key = self.input['key']
+            place = self.input['place']
+            description = self.input['description']
+            pic_url = self.input['picUrl']
+            start_time = self.input['startTime']
+            end_time = self.input['endTime']
+            book_start = self.input['bookStart']
+            book_end = self.input['bookEnd']
+            total_tickets = self.input['totalTickets']
+            remain_tickets = self.input['totalTickets']
+            status = self.input['status']
+            try:
+                new = Activity(name=name, key=key, place=place, 
+                description=description, pic_url=pic_url, start_time=start_time, 
+                end_time=end_time, book_start=book_start, book_end=book_end, 
+                total_tickets=total_tickets, remain_tickets=remain_tickets, status=status)
+                new.save()
+                return new.id
+            except:
+                raise ValidateError('Create activity error!')
+        else:
+            raise ValidateError('Please login!')
+
+# #删除活动
+class activityDelete(APIView):
+    def post(self):
+        self.check_input('id')
+        id = self.input['id']
+        activity = Activity.objects.filter(id__exact = id)
+        if activity:
+            try:
+                activity.delete()
+            except:
+                raise ValidateError("System error!")
+        else:
+            raise ValidateError("ID does not exit!")
